8cbd17ced0ac88335e85f93915853d141cd882a0
Chris Jin <78185795+fscj-4raxhel@users.noreply.github.com>
2024-05-03 16:34:26 -0400

    Completed Exercise 1 Chapter 4 about accumated round-off errors
    It seems that a whole binary fraction is less likely to have this kind of erros.

---
+++ src/exercises/chapter4/Exercise1.java
@@ -0,0 +1,34 @@
+package exercises.chapter4;
+
+/**
+ * Rewrite Example_4_1 to sum ⅓ three hundred times, ¼ four hundred times and ⅛ eight hundred times. Comment on the results.
+ * 
+ * This class demonstrates the accumulated round-off error with different examples
+ */
+
+public class Exercise1 {
+
+    public Exercise1(){
+        System.out.println("Sum 1/3 three hundred times is: " + sumDouble(1.0 / 3, 300));
+        System.out.println("Sum 1/4 four undred times is: " + sumDouble(1.0 / 4, 400));
+        System.out.println("Sum 1/8 eight hundred times is: " + sumDouble(1.0 / 8, 800));
+    }
+
+    /**
+     * This method sum a given value x the t times and print the result.
+     * There will be accumalated round-off error.
+     * @param x
+     * @param times
+     */
+    public double sumDouble(double x, int t){
+        double sum = 0;
+
+        for (int i = 0; i < t; i++) {
+            sum += x;
+        }
+        return sum;
+    }
+    public static void main(String[] args){
+        Exercise1 e1 = new Exercise1();
+    }
+}
